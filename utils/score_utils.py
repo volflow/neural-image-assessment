b475833bf6476a1ad396f64b0f542c8554d4c116
@volflow <valentin@valentin-wolf.de>
2018-03-26 23:46:44 +0200

    vectorized score metrics

--- utils/score_utils.py
+++ utils/score_utils.py
@@ -3,12 +3,15 @@ import numpy as np
 # calculate mean score for AVA dataset
 def mean_score(scores):
     si = np.arange(1, 11, 1)
-    mean = np.sum(scores * si)
+    mean = np.sum(si * scores,axis=-1)
     return mean
 
 # calculate standard deviation of scores for AVA dataset
 def std_score(scores):
-    si = np.arange(1, 11, 1)
+    if len(scores.shape) < 3:
+        scores = np.expand_dims(scores, axis=0)
+    si = np.ones((scores.shape[0],10)) * np.arange(1, 11, 1)
     mean = mean_score(scores)
-    std = np.sqrt(np.sum(((si - mean) ** 2) * scores))
-    return std
+    a = (si - mean.T) ** 2
+    std = np.sqrt(np.sum((a) * scores,axis=-1))
+    return std[0]
