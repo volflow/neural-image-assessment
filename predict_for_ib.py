f60e7ea914d6c802ae5e8b48456933854a9101f7
@volflow <valentin@valentin-wolf.de>
2018-04-22 00:05:14 +0200

    refractored

--- predict_for_ib.py
+++ predict_for_ib.py
@@ -1,5 +1,7 @@
 csv_path = '/home/ubuntu/data/ib-urls/' #'/Users/valentinwolf/data/ib-urls-test/'
-
+CHUNK_SIZE = 256
+BATCH_SIZE = 64
+MAX_WORKERS = 14
 import numpy as np
 import pandas as pd
 import glob
@@ -29,46 +31,53 @@ def resize(img,target_size):
 
 def download_img(url,target_size=(224,224)):
     """
-    downloads image from url; resizes image to target_size; retruns image
+    downloads image from url; resizes image to target_size; returns image
+    returns -1 if download was not successfull
+    TODO: find pythonic way to write try: with:
     """
     try:
-        downloaded_img = req.urlopen(url)
+        with req.urlopen(url) as downloaded_img:
+            try:
+                with Image.open(downloaded_img) as img:
+                    return resize(img,target_size)
+            except OSError:
+                print('\nfailed to download {}'.format(url))
+                return -1
+
     except ValueError:
-        print('failed open {}, unkown url type'.format(url))
+        print('\nunkown url type, failed open {}'.format(url))
         return -1
-
-    try:
-        img = Image.open(downloaded_img)
-    except OSError:
-        print('failed to download {}'.format(url))
-        return -1
-
-    img = resize(img,target_size)
-    return img
 
 def inference_batchwise(model,batch):
     x = preprocess_input2(batch)
     scores = model.predict(x, batch_size=len(batch), verbose=0)
     return scores
 
+def gen_batches(li, batch_size):
+    """
+    returns list of lists of size batch_size, with the elemnts of li
+    if len(list) is not divisible by n the last list may be smaller than
+    batch_size
+    TODO: make this a generator (yield)
+    """
+    return [li[i:i+batch_size] for i in range(0, len(li), batch_size)]
 
 def inference_from_urls(model,imgs,batch_size=32):
     target_size = (224, 224)
     with tf.device('/CPU:0'):
         total_imgs = len(imgs)
         score_list = []
-        batches = int(np.ceil(total_imgs / batch_size))
+        total_predicted = 0
 
-        print('\rEvaluating: {}/{} '.format(
+        print('\rPredicting: {}/{} '.format(
                 0,total_imgs), end='')
 
-        for b in range(batches):
-            batch = imgs[b*batch_size:(b+1)*batch_size]
+        for batch in gen_batches(imgs,batch_size=batch_size):
             x = np.zeros((len(batch), 224, 224, 3))
 
             download_start = time.time()
-            MAX_WORKERS = 14
-            with Pool(MAX_WORKERS) as p:
+            max_workers = MAX_WORKERS
+            with Pool(max_workers) as p:
                 image_list = p.map(download_img,batch)
 
                 failed_imgs = []
@@ -82,15 +91,17 @@ def inference_from_urls(model,imgs,batch_size=32):
             download_time = (time.time() - download_start) / batch_size
             inference_start = time.time()
             scores = inference_batchwise(model,x)
-            # incicate failed predictions by score of [-1,0,0,...] / mean -1
-            scores[failed_imgs] = 0 
+
+            # incicate failed predictions by score of [-1,0,0,...] => mean -1
+            scores[failed_imgs] = 0
             scores[failed_imgs,0] = -1
+
             del x
             inference_time = (time.time() - inference_start) / batch_size
             score_list.append(scores)
-            print('\rEvaluating: {}/{} Download: {:.2f}s Inference {:.2f}s'.format(
-                b*batch_size+len(batch),total_imgs,download_time, inference_time), end='')
-
+            total_predicted += len(batch)
+            print('\rPredicting: {}/{} Download: {:.2f}s Inference {:.2f}s'.format(
+                total_predicted,total_imgs,download_time, inference_time), end='')
     return np.vstack(score_list)
 
 if __name__ == "__main__":
@@ -100,31 +111,31 @@ if __name__ == "__main__":
     model = evaluate.nasnet()
     csv_file_paths = sorted(glob.glob(csv_path+'/*.csv'))
     print(csv_file_paths)
+    total_predicted = 0
     for csv_file_path in csv_file_paths:
         print("Reading new file: " + csv_file_path)
 
         csv_file = pd.read_csv(csv_file_path,sep=';',header=None,names=['id','url','score'])
         csv_file = csv_file.fillna(0.) # set nan values in score col to 0
-
+        rows_in_file = csv_file.shape[0]
         # find first row with score 0
         index_list = csv_file.index[csv_file['score']==0.]
         if len(index_list) == 0:
             print("everything already predicted")
+            total_predicted += rows_in_file
             continue
-        offset = index_list[0]
 
+        chunk_size = CHUNK_SIZE
+        offset = index_list[0]
+        total_predicted += offset
         urls_list = csv_file['url'].tolist()[offset:]
         total_urls = len(urls_list)
         print("Found {} urls with score 0. Predicting ...".format(total_urls))
-
-        chunk_size = 64
-        chunks = int(np.ceil(total_urls / chunk_size))
-        for i in range(chunks):
-            start_i = i*chunk_size
-            end_i = (i+1)*chunk_size
-            chunk = urls_list[start_i:end_i]
-            scores = inference_from_urls(model,chunk,batch_size=64)
+        for chunk in gen_batches(urls_list, chunk_size):
+            scores = inference_from_urls(model,chunk,batch_size=BATCH_SIZE)
             mean_scores = mean_score(scores)
-            csv_file.loc[start_i+offset:end_i+offset-1,'score'] = mean_scores
-            print("\nSaving progress to {}".format(csv_file_path))
+            csv_file.loc[offset:offset+len(chunk)-1,'score'] = mean_scores
+            offset += len(chunk)
+            total_predicted += len(chunk)
+            print("\n{}/{} | Total: {} Saving to {}".format(offset,rows_in_file,total_predicted,csv_file_path))
             csv_file.to_csv(csv_file_path, sep=';', header=False)
